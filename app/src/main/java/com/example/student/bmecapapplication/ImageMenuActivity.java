9a231650f6071f0ed420de666e9beb2919414f06
Charles <cp8tk@virginia.edu>
2018-02-19 03:59:08 -0500

    Image Scrolling Screen
    progress on image scroll screen

---
+++ app/src/main/java/com/example/student/bmecapapplication/ImageMenuActivity.java
@@ -0,0 +1,17 @@
+package com.example.student.bmecapapplication;
+
+/**
+ * Created by Charles Pritchett on 2/19/2018.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageMenuActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_menu);
+    }
+
+}
---
+++ app/src/main/java/com/example/student/bmecapapplication/SavedScrollActivity.java
@@ -0,0 +1,17 @@
+package com.example.student.bmecapapplication;
+
+/**
+ * Created by student on 2/19/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SavedScrollActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
